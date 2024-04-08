577ddff64053b3491733dd092b70fcc955cba4c1
reckyy <srreki98@gmail.com>
2024-04-08 18:13:29 +0900

    ⚡️ feat: キャラクターの情報を表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     fetchCharacters();
   }, []);
@@ -11,9 +13,43 @@ function App() {
     const apiUrl = "https://narutodb.xyz/api/character";
 
     const result = await axios.get(apiUrl);
+    setCharacters(result.data.characters);
     console.log(result);
   };
-  return <div className="App">hello world</div>;
+  return (
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((character) => {
+            return (
+              <div className="card" key={character.id}>
+                <img
+                  src={
+                    character.images[0] != null
+                      ? character.images[0]
+                      : "dummy.png"
+                  }
+                  alt="character"
+                  className="card-image"
+                />
+                <div className="card-content">
+                  <h3 className="card-title">{character.name}</h3>
+                  <p className="card-description">
+                    {character.debut?.appearsIn ?? "なし"}
+                  </p>
+                  <div className="card-footer">
+                    <span className="affiliation">
+                      {character.personal?.affiliation ?? "なし"}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
