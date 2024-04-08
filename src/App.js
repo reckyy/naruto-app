2f114c2dd40e7da16602b29b4e1556a641ceb506
reckyy <srreki98@gmail.com>
2024-04-08 17:34:16 +0900

    ⚡️ feat: characterを取得

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (<div className="App">hello world</div>);
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+
+    const result = await axios.get(apiUrl);
+    console.log(result);
+  };
+  return <div className="App">hello world</div>;
 }
 
 export default App;
